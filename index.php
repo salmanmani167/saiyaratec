81385391ae283a06f7d8a4a9a3aaa7e818c43a43
salmanmani167 <salmanmani167@gmail.com>
2023-05-27 22:07:56 +0500

    ok

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Git ttt</title>
+  <title>Arsha Bootstrap Template - Index</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -86,9 +86,8 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>SEO</h1>
-          <h2>fwewefw</h2>
-          <h3>Link Buildings are a popular technique used in search engine optimization (SEO) to improve the ranking of a website in search engine results pages (SERPs).</h3>
+          <h1>Better Solutions For Your Business</h1>
+          <h2>We are team of talented designers making websites</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
             <!-- <a href="https://www.youtube.com/watch?v=K1QICrgxTjA" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
